5da4f5fc2f8af88fb6c7ad33052468c6072a1228
PedroItaloQ <peedroitalo2906@gmail.com>
2024-05-14 21:32:57 -0300

    Updates

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+ import { createStackNavigator } from '@react-navigation/stack';
+ import React from 'react';
+ import ProdutoForm from './view/ProdutoForm';
+ import ProdutoLista from './view/ProdutoLista'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ export default function App() {
+     return (
+     <NavigationContainer>
+         <Stack.Navigator initialRouteName='ListaProd'>
+         <Stack.Screen name='ListaProd'
+             options={{title: 'Listagem de Produtos'}}
+             component={ProdutoLista} />
+         <Stack.Screen name='NovoProd'
+             options={{title: 'Novo Produto'}}
+             component={ProdutoForm} />
+         </Stack.Navigator>
+     </NavigationContainer>
+     );
+ }
+                         
